1f8f5176dbec6a445595af2ddd3cdb062503f0ed
Ace <bonistudio.core@gmail.com>
2026-07-07 13:28:04 -0700

    Fix GitHub Pages project deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/Bonistudio.red/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 950,
